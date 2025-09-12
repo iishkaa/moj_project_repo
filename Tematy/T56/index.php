8d6a40d89e87b59835ebfc1d8bc3b420db4fd9fb
iishkaa <iishkaa222@gmail.com>
2025-09-12 11:17:16 +0200

    T56 - (poprawka)

--- Tematy/T56/index.php
+++ Tematy/T56/index.php
@@ -8,7 +8,11 @@
 <body>
 <h1>Zadanie T56</h1>
 <h3>Autor: Michalina Wolna 2p_2</h3>
-
+<pre>Utwórz skrypt, który będzie zapisywał opinie użytkowników w pliku tekstowym opinie.txt.
+    Na stronie wyświetl formularz, który pozwoli na wpisanie opinii. Nowe opinie powinny być dopisywane
+    do pliku i umieszczane na jego końcu.
+    Dotychczasowe opinie zapisane w pliku tekstowym powinny zostać wyświetlone na stronie i powinny
+    być dostępne dla innych jej użytkowników.</pre>
 <div>
     <form action="" method="post">
         <p><b>Dodaj swój komentarz na temat globalnego ocieplenia</b><br>
@@ -17,6 +21,17 @@
         <input type="submit" value="Wyślij">
     </form>
 </div>
+<!-- isset() - sprawdza czy zmienna (np $_POST['komentarz'] - zostala ustawiona -->
+<!--substr() = ucina komentarz do ograniczenia np 255 znakow -->
+<!--strip_tags() - usuwa wszystkie znaczniki HTML i PHP -->
+<!--fopen() - otwiera plik, w trybie a - append = dopisywanie na koncu-->
+<!--//fwrite() - zapisuje do pliku-->
+<!--//fclose() - zamyka plik-->
+<!--file_exists() - sprawdza czy plik istnieje-->
+<!--file_get_contents() - wczytuje cala wartosc pliku jako ciag znakow-->
+<!--trim() - usuwa biale znaki z poczatku i konca (np spacje, nowe linie)-->
+<!--htmlspecialchars() - zabezpiecza kod przed interpretacja jako HTML-->
+<!--nl2br() - zamienia znaki nowej linii \n na znaczniki HTML->-->
 
 <b>Dodane opinie:</b>
 <div>
@@ -50,14 +65,3 @@
 </div>
 </body>
 </html>
-//isset() - sprawdza czy zmienna (np $_POST['komentarz'] - zostala ustawiona
-//substr() = ucina komentarz do ograniczenia np 255 znakow
-//strip_tags() - usuwa wszystkie znaczniki HTML i PHP
-//fopen() - otwiera plik, w trybie a - append = dopisywanie na koncu
-//fwrite() - zapisuje do pliku
-//fclose() - zamyka plik
-//file_exists() - sprawdza czy plik istnieje
-//file_get_contents() - wczytuje cala wartosc pliku jako ciag znakow
-//trim() - usuwa biale znaki z poczatku i konca (np spacje, nowe linie)
-//htmlspecialchars() - zabezpiecza kod przed interpretacja jako HTML
-//nl2br() - zamienia znaki nowej linii \n na znaczniki HTML
