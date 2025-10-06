0770a69d2633c04679ff36726795783351026474
iishkaa <iishkaa222@gmail.com>
2025-10-06 14:17:40 +0200

    T60a

---
+++ Tematy/T60a/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>praca z bazą danych</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Zadanie T60a - praca z bazą danych</h1>
+<h2>Autor: Michalina Wolna 3p_2</h2>
+<hr>
+<section>
+    Tym razem zadanie jest bardziej złożone. Oto co należy zrobić:
+    <ul>
+        <li>Utwórz bazę danych o nazwie 3p_2_baza_pracownikow.</li>
+        <li>Do zadania dołączony został plik zawierający dane 114 pracowników -
+            dokonaj konwersji tych danych do formaty txt (uzyskaj plik z danymi pracownicy.txt.</li>
+        <li>Na utworzonej stronie projektu znajduje się przycisk "Utwórz tabelę", który w bazie 3p_2_baza_pracownikow tworzy tabelę pracownicy.</li>
+        <li>Drugi przycisk "Załaduj dane" dodaje dane z pliku tekstowego pracownicy.txt do tabeli pracownicy.</li>
+        <li>Trzeci przycisk wyświetla dane z tabeli pracownicy w postaci tabelarycznej.</li>
+    </ul>
+</section>
+<hr>
+<section><br>
+    <input type="submit" name="tabela" value="Utwórz tabelę">
+    <input type="submit" name="zaladuj" value="Załaduj dane">
+    <input type="submit" name="wyswietl" value="Wyświetl dane">
+</section>
+<?php
+
+?>
+
+</body>
+</html>
